0c7af80175da7536073e468a594b88c13d0d59e5
benmoyal1 <benmoyal2013@gmail.com>
2024-12-09 17:10:59 +0200

    prod

--- src/code/ExperimentClasses/timeTable.js
+++ src/code/ExperimentClasses/timeTable.js
@@ -1,5 +1,7 @@
-const FASTMODE = true;
-const MINSLIDES = true;
+const FASTMODE = false;
+const MINSLIDES = false;
+console.log("Updated 9/12 17:10");
+
 const expConfigure = {
   fastmode: FASTMODE,
   minSlides: MINSLIDES,
