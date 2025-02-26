6fe985acdaee10c69cefbc5908deeada62a4e11a
benmoyal1 <benmoyal2013@gmail.com>
2025-02-26 15:59:03 +0200

    a

--- src/code/api/dataFromServer.js
+++ src/code/api/dataFromServer.js
@@ -1,19 +1,19 @@
 async function getParticipantNum() {
-    try {
-        const devUrl = 'http://localhost:3000/';
-        const serverUrl = 'https://express-backend-exp.vercel.app/data/participantCounter';
-        const response = await fetch(serverUrl, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        const result = await response.json(); // Parse response as JSON
-        console.log(result)
-        return result;
-        
-    } catch (error) {
-        console.error('Error:', error);
-    }
+  try {
+    const devUrl = "http://localhost:3000/";
+    const serverUrl =
+      "https://express-backend-exp.vercel.app/data/participantCounter";
+    const response = await fetch(serverUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const result = await response.json(); // Parse response as JSON
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
-export {getParticipantNum};
+export { getParticipantNum };
